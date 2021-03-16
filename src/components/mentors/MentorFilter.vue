46fa9adfcff914414940af986b91936615759ea9
prototowb <prototowb@gmail.com>
2021-03-17 00:56:59 +0100

    border tweaks

--- src/components/mentors/MentorFilter.vue
+++ src/components/mentors/MentorFilter.vue
@@ -1,6 +1,6 @@
 <template>
   <base-card>
-    <h2>Find your Mentor</h2>
+    <h2>Find your Mentor!</h2>
     <span class="filter-option">
       <input type="checkbox" id="frontend" checked @change="setFilter" />
       <label for="frontend">Frontend</label>
