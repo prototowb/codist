985c8f9e88f2d82579d22410ff386f09ff4b02ec
prototowb <prototowb@gmail.com>
2021-03-17 00:15:00 +0100

    include dist again

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <nav>
-      <h1><router-link to="/">Find a Mentor</router-link></h1>
+      <h1><router-link to="/">Codist</router-link></h1>
       <ul>
         <li>
           <router-link to="/mentors">All Mentors</router-link>
