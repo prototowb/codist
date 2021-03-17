81d73ad1d4f9ed7c04616db94fd329f74cbb8ad3
prototowb <prototowb@gmail.com>
2021-03-17 14:44:05 +0100

    mentor details

--- src/store/modules/mentors.js
+++ src/store/modules/mentors.js
@@ -7,7 +7,7 @@ export default {
                   id: 'c1',
                   firstName: 'Tobias',
                   lastName: 'Rauer',
-                  areas: ['frontend', 'backend', 'career'],
+                  areas: ['frontend', 'career'],
                   description:
                     "I'm Tobias and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
                   hourlyRate: 30
@@ -16,7 +16,7 @@ export default {
                   id: 'c2',
                   firstName: 'Julie',
                   lastName: 'Jones',
-                  areas: ['frontend', 'career'],
+                  areas: ['frontend', 'backend', 'career'],
                   description:
                     'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
                   hourlyRate: 30
